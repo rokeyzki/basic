89f77632521279e0e4b0571d1700f0eb7ce0eb83
rokeyzki <rokeyzki@gmail.com>
2016-10-10 16:31:27 +0800

    adjust

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: {
     'one/index': './src/module/es6/entry',
-    'one/two/other': './src/module/test/entry'
+    'one/two/other': './src/module/vue/entry'
   },
 
   output: {
@@ -10,6 +12,7 @@ module.exports = {
   },
 
   externals: {
+    'vue': 'Vue',
     // 'react': 'React',
     // 'react-dom': 'ReactDOM',
     // 'react-router': 'ReactRouter',
@@ -18,32 +21,27 @@ module.exports = {
   },
 
   module: {
-    // preLoaders: [
-    //   {
-    //     test: /\.js$/, // 只针对js文件
-    //     loader: 'eslint', // 指定启用eslint-loader
-    //     exclude: /node_modules/
-    //   }
-    // ],
-
     loaders: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules|build)/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015'],
           // plugins: ['antd']
         }
       }
-      // ,{
-      //   test: /\.css$/,
-      //   loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader?{browsers:["last 2 versions", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"]}')
-      // }
-      // ,{
-      //   test: /\.less$/,
-      //   loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader?{browsers:["last 2 versions", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"]}!less-loader')
-      // }
+
+      ,{
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader?{browsers:["last 2 versions", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"]}')
+      }
+
+      ,{
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader?{browsers:["last 2 versions", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"]}!less-loader')
+      }
+
       // ,{
       //   test: /\.(jpe?g|png|gif|svg)$/i,
       //   loaders: [
@@ -57,26 +55,20 @@ module.exports = {
       // }
     ],
 
-    // postLoaders: [ // 为了兼容IE8
-    //   {
-    //     test: /\.js$/,
-    //     loaders: ['es3ify-loader'], // sudo npm install es3ify-loader
-    //   },
-    // ],
+    postLoaders: [ // 为了兼容IE8
+      // {
+      //   test: /\.js$/,
+      //   loaders: ['es3ify-loader'], // sudo npm install es3ify-loader
+      // },
+    ],
   },
 
   plugins: [
-    // new ExtractTextPlugin('style/[name].min.css'),
+    new ExtractTextPlugin('style/[name].min.css'),
     // new BannerPlugin('This file is created by Charles Lim'),
     // new CommonsChunkPlugin('script/common.min.js'),
     // new UglifyJsPlugin({compress:{warnings: false}})
   ],
-
-  // eslint: {
-  //   failOnWarning: true, // eslint报warning了就终止webpack编译
-  //   failOnError: true, // eslint报error了就终止webpack编译
-  //   cache: true, // 开启eslint的cache，cache存在node_modules/.cache目录里
-  // },
 
   // imagemin: {
   //   gifsicle: { interlaced: false },
