2372828ac0994704c0e52f3e7b8783f90d9b40bf
rokeyzki <rokeyzki@gmail.com>
2016-10-17 18:30:53 +0800

    adjust

--- src/module/design/foo1/demo.js
+++ src/module/design/foo1/demo.js
@@ -5,7 +5,9 @@ const showHeight = $(window).height();
 console.log(`当前窗口可视宽度为：${showWidth} px`);
 console.log(`当前窗口可视高度为：${showHeight} px`);
 
-const fooNode = document.querySelector('#foo');
-console.dir(fooNode);
-fooNode.style.setProperty('width', `${showWidth}px`);
-fooNode.style.setProperty('height', `${showHeight}px`);
+// const fooNode = document.querySelector('#foo');
+// console.dir(fooNode);
+// fooNode.style.setProperty('width', `${showWidth}px`);
+// fooNode.style.setProperty('height', `${showHeight}px`);
+
+$('#foo').css('width', `${showWidth}px`).css('height', `${showHeight}px`);
