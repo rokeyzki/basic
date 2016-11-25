4df2956cb483d67ed848401563e41f56c9249252
林川淇 <charles@linchuanqideMacBook-Pro.local>
2016-11-26 00:36:16 +0800

    antd

---
+++ src/module/manage/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'antd/dist/antd.less';
+
+import Hello from './components/hello/main';
+
+ReactDOM.render(
+  <Hello />,
+  document.querySelector('#app')
+);
