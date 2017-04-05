4b511444dd90351bc7e84f79eccbbe76192520d9
rokeyzki <rokeyzki@gmail.com>
2017-04-05 22:22:23 +0800

    module

--- src/module/react/appBase.jsx
+++ src/module/react/appBase.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// 自定义helper文件测试 start
+import test from './bin/moduleB';
+console.log(test('world: helper test'));
+// 自定义helper文件测试 end
+
 import HelloBase from './components/hello_base';
 
 ReactDOM.render(
---
+++ src/module/react/bin/moduleA.js
@@ -0,0 +1,6 @@
+// CommonJS 规范
+function foo() {
+  return 'hello ';
+}
+
+module.exports = foo;
