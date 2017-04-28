4dbc0cc72448764ebd9394c516e878b00a8d41c5
rokeyzki <rokeyzki@gmail.com>
2017-04-28 17:29:49 +0800

    es6

---
+++ src/es6/type/give.js
@@ -0,0 +1,43 @@
+// 变量的解构赋值
+
+const [a, b, c] = [1, 2, 3];
+console.log(a, b, c);
+
+const { aa, bb } = { bb: 'bbb', aa: 'aaa' };
+console.log(aa); // "aaa"
+console.log(bb); // "bbb"
+
+const [foo, [[bar], baz]] = [1, [[2], 3]];
+console.log(foo, bar, baz);
+
+const [, , third] = [1, 2, 3];
+console.log(third);
+
+const [head, ...tail] = [1, 2, 3, 4];
+console.log(head); // 1
+console.log(tail); // [2, 3, 4]
+
+const [x, y] = [1, 2, 3];
+console.log(x); // 1
+console.log(y); // 2
+
+const [a1, [b1], d1] = [1, [2, 3], 4];
+console.log(a1); // 1
+console.log(b1); // 2
+console.log(d1); // 4
+
+// 解构赋值的用途
+
+// DEMO 1：交换变量的值
+
+// DEMO 2：从函数返回多个值
+
+// DEMO 3：函数参数的定义
+
+// DEMO 4：提取对象数据
+
+// DEMO 5：指定函数参数的默认值
+
+// DEMO 6：遍历Map结构
+
+// DEMO 7：输入模块的指定方法
