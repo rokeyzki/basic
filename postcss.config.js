7b9093541513498ca19f6b669aabc8a7c046bed8
rokeyzki <rokeyzki@gmail.com>
2017-06-22 10:41:30 +0800

    config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const autoprefixer = require('autoprefixer'); // 补全浏览器前缀
 const cssgrace = require('cssgrace'); // 生成兼容旧浏览器的各种 Hack、提供 CSS3 Polyfill
 const cssnano = require('cssnano'); // CSS 压缩合并
@@ -6,6 +8,6 @@ module.exports = {
   plugins: [
     autoprefixer,
     cssgrace,
-    cssnano,
+    new cssnano({ zindex: false }),
   ],
 };
