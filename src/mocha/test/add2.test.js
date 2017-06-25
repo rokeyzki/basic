482567cdea972cc3e09f6da668b4c12d04d0dff6
rokeyzki <rokeyzki@gmail.com>
2017-06-25 16:39:12 +0800

    test

--- src/mocha/test/add2.test.js
+++
@@ -1,8 +0,0 @@
-var add = require('../add.js');
-var expect = require('chai').expect;
-
-describe('加法函数的测试2', function() {
-  it('1 加 0 应该等于 1', function() {
-    expect(add(1, 0)).to.be.equal(1);
-  });
-});
