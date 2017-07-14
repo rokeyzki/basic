6d8c93b0caabe531902d8c607882b4ab3d9c38c7
rokeyzki <rokeyzki@gmail.com>
2017-07-14 15:01:14 +0800

    test

--- src/react/components/hello_redux/test/containers/hoc.jsx
+++ src/react/components/hello_redux/test/containers/hoc.jsx
@@ -8,17 +8,9 @@ import { Provider } from 'react-redux';
 import store from '../../store';
 
 export default function ppHOC(WrappedComponent) {
-  return class PP extends React.Component {
-    componentWillMount() {
-      console.log('HOC 高阶组件');
-    }
-
-    render() {
-      return (
-        <Provider store={store}>
-          <WrappedComponent {...this.props} />
-        </Provider>
-      );
-    }
-  };
+  return (...props) => (
+    <Provider store={store}>
+      <WrappedComponent {...props} />
+    </Provider>
+  );
 }
