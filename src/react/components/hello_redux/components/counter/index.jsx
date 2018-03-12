0474b6363059d7c1301bdcc6db30f6376e8517f4
rokeyzki <rokeyzki@gmail.com>
2018-03-12 23:32:12 +0800

    flow

--- src/react/components/hello_redux/components/counter/index.jsx
+++ src/react/components/hello_redux/components/counter/index.jsx
@@ -1,30 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// @flow
 
-const Counter = ({
-  value,
-  color,
-  onIncreaseClick,
-  onReduceClick,
-  onBlackClick,
-  onRedClick,
-}) => (
+import React from 'react';
+
+type Props = {
+  value: number,
+  color: string,
+  onIncreaseClick: any,
+  onReduceClick: any,
+  onBlackClick: any,
+  onRedClick: any,
+};
+
+const Counter = (props: Props) => (
   <div>
-    <p style={{ color }}>{value}</p>
-    <button onClick={onIncreaseClick}>增加</button>
-    <button onClick={onReduceClick}>减少</button>
-    <button onClick={onBlackClick}>黑色</button>
-    <button onClick={onRedClick}>高亮</button>
+    <p style={{ color: props.color }}>{props.value}</p>
+    <button onClick={props.onIncreaseClick}>增加</button>
+    <button onClick={props.onReduceClick}>减少</button>
+    <button onClick={props.onBlackClick}>黑色</button>
+    <button onClick={props.onRedClick}>高亮</button>
   </div>
 );
-
-Counter.propTypes = {
-  value: PropTypes.number.isRequired,
-  color: PropTypes.string.isRequired,
-  onIncreaseClick: PropTypes.func.isRequired,
-  onReduceClick: PropTypes.func.isRequired,
-  onBlackClick: PropTypes.func.isRequired,
-  onRedClick: PropTypes.func.isRequired,
-};
 
 export { Counter };
