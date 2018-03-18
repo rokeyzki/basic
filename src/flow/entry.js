5362d3a2d143851aab07d3696013985a7d053e41
rokeyzki <rokeyzki@gmail.com>
2018-03-18 23:14:04 +0800

    flow

--- src/flow/entry.js
+++ src/flow/entry.js
@@ -2,3 +2,12 @@
 
 const str: string = 'hello world!';
 console.log(str);
+
+function foo(x: ?number): string {
+  if (x) {
+    return `${x}`;
+  }
+  return 'default string';
+}
+
+foo(3);
--- src/react/components/hello_redux/components/counter/index.jsx
+++ src/react/components/hello_redux/components/counter/index.jsx
@@ -1,23 +1,29 @@
 // @flow
-
 import React from 'react';
 
 type Props = {
   value: number,
   color: string,
-  onIncreaseClick: any,
-  onReduceClick: any,
-  onBlackClick: any,
-  onRedClick: any,
+  onIncreaseClick: mixed,
+  onReduceClick: mixed,
+  onBlackClick: mixed,
+  onRedClick: mixed,
 };
 
-const Counter = (props: Props) => (
+const Counter = ({
+  value,
+  color,
+  onIncreaseClick,
+  onReduceClick,
+  onBlackClick,
+  onRedClick,
+}: Props) => (
   <div>
-    <p style={{ color: props.color }}>{props.value}</p>
-    <button onClick={props.onIncreaseClick}>增加</button>
-    <button onClick={props.onReduceClick}>减少</button>
-    <button onClick={props.onBlackClick}>黑色</button>
-    <button onClick={props.onRedClick}>高亮</button>
+    <p style={{ color }}>{value}</p>
+    <button onClick={onIncreaseClick}>增加</button>
+    <button onClick={onReduceClick}>减少</button>
+    <button onClick={onBlackClick}>黑色</button>
+    <button onClick={onRedClick}>高亮</button>
   </div>
 );
 
