53d02ab814d50007e7b81ebe2aca1a894697fde6
rokeyzki <rokeyzki@gmail.com>
2018-05-24 00:17:55 +0800

    add feat

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -4,11 +4,23 @@ import "isomorphic-fetch";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-// import { Hello } from "./components/Hello";
-import MainDemo from "./components/main-demo";
+import { Hello } from "./components/Hello";
+// import MainDemo from "./components/main-demo";
+
+const bar = () => 321;
+
+function baz(a, b) {
+    console.log(a, b);
+}
 
 ReactDOM.render(
-    // <Hello compiler="TypeScript" framework="React" />,
-    <MainDemo />,
+    <Hello
+        compiler="TypeScript"
+        framework="React"
+        test={{ foo: 123 }}
+        bar={bar}
+        baz={baz}
+    />,
+    // <MainDemo />,
     document.querySelector("#app"),
 );
--- src/ts/components/Hello.tsx
+++ src/ts/components/Hello.tsx
@@ -1,11 +1,28 @@
 import * as React from "react";
 
-export interface InterProps { compiler: string; framework: string; }
+interface ImterTest {
+    foo: number;
+}
+
+type FuncBar = () => number;
+
+type FuncBaz = (a: string, b: string) => void;
+
+interface InterProps {
+    compiler: string;
+    framework: string;
+    test: ImterTest;
+    bar: FuncBar;
+    baz: FuncBaz;
+}
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<InterProps, {}> {
     public render() {
+        console.log(this.props.bar());
+        this.props.baz("A", "B");
+        console.log(this.props.test.foo);
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
